cb779df05a939704e477c21ac19d7911201081a8
Parker <burnettparker@gmail.com>
2019-06-17 20:37:27 -0600

    I tried to work on things...but Im tired...

---
+++ fortnite.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+    "fmt"
+    "flag"
+    "os"
+)
+
+func main() {
+    // This is where the dancing will happen...possibly
+    // Delay the frame for a bit
+    delay := flag.Int("delay for 75ms", 75, "frame delay in ms")
+}
