e4a2b786a9f7a71de112191f563e52e60c08cd86
Parker <burnettparker@gmail.com>
2019-06-19 20:16:46 -0600

    Looked at some things, did some reading, gave credit

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ A terminal script to have a dancing fortnite emote.
 Well, I saw these in a copy pasta on reddit and figured I would try and animate them. I started with using just a bash script to print out all the .txt files.
 BUT! Then I remembered about parrot.live and parrot-terminal and figured I would try and do the same thing!
 ...So that is what I am going to do....
+
+# Big Thanks
+
+Terminal thanks to termbox-go(https://www.github.com/nsf/termbox-go)
--- fortnite.go
+++ fortnite.go
@@ -4,10 +4,38 @@ import (
     "fmt"
     "flag"
     "os"
+
+
+    isatty "github.com/mattn/go-isatty" // For all the terminal stuff
 )
 
 func main() {
     // This is where the dancing will happen...possibly
     // Delay the frame for a bit
     delay := flag.Int("delay for 75ms", 75, "frame delay in ms")
+    orientation := flag.String("orientation", "regular")
+    flag.Parse()
+
+    if !isatty.IsTerminal(os.Stdout.Fd()) && !isatty.IsCygwinTerminal(os.Stdout.Fd()){
+        fmt.Sprintf(os.Stderr, "Error: %s must be ran in a terminal\n", filepath.Base(os.Args[0]))
+        os.Exit(1)  // Exit out after Erroring
+    }
+
+    err := termbox.Init()
+    if err != nil {
+        panic(err)
+    }
+    defer termbox.Close()
+
+    eventQ := make(chan termbox.Event)
+    go func() {
+        for {
+            eventQ <- termbox.PollEvent()
+        }
+    }()
+
+    termbox.SetOutputMode(termbox.Output256)
+    loopIdx := 0
+    draw(*orientation)
+
 }
